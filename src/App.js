4e3ac6b51cf94fbd21bac27c4e41478767c67c75
charankaramala <charankaramala49@gmail.com>
2024-11-15 20:57:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,53 +2,58 @@ import { useState } from "react"
 
 function App()
 {
-  let [todo,updatetodo]=useState([{
-     "task":"hello java"
-  },
-{
-  "task":"hello python"
-}])
-function Addnew()
-{
-  let a=document.getElementById("data").value 
-  let newtodo=[
-    ...todo,
+  let [todo,updatetodo]=useState([
     {
-      "task":a
-    }
-  ]
-  updatetodo(newtodo)
-}
-function Deletetodo(value1)
-{
-   let freshtodo=todo.filter((v)=>
+      'id':1,
+       "task":"i want to learn java"
+    },
   {
-    return(
-      v.task!==value1
-    )
-  })
-  updatetodo(freshtodo)
-}
-return(
-  <div className="container mt-5 w-50">
-       <div className="input-group">
-        <input type="text" className="form-control" id="data"></input>
-        <button className="btn btn-primary"onClick={()=>{Addnew()}}>Addme</button>
-       </div>
-       <ul className="list-group mt-3">
-              {
-                todo.map((e)=>
-                {
-                  return(
-                    <li className="list-group-item">
-                    <p>{e.task}</p>
-                    <button className="btn btn-danger"onClick={()=>{Deletetodo(e.task)}}>clearme</button>
-                  </li>
-                  )
-                })
-              }
-       </ul>
-  </div>
-)
+    'id':2,
+       "task":"i want to learn python"
+  }])
+  function Addme()
+  {
+    let data1=document.getElementById('data').value 
+    console.log(data1)
+    updatetodo([
+      ...todo,
+      {
+      "task":data1
+    }
+    ])
+  }
+  function Delete(id)
+  {
+    console.log(id)
+    let data2=todo.filter((e)=>{
+      if(e.task!=id)
+      {
+        return e.task
+      }
+    })
+    updatetodo(data2)
+  }
+  return(
+    <div  className="container w-50 mt-5">
+        <div className="input-group">
+          <input className="form-control" type="text"id="data"></input>
+          <button className="btn btn-primary"onClick={()=>{Addme()}}>Add</button>
+        </div>
+      <ul class="list-group mt-4">
+        {
+          todo.map((e)=>
+          {
+            return(
+            <li class="list-group-item">
+              <p>{e.task}</p>
+              <button onClick={()=>{Delete(e.task)}}>del</button>
+            </li>
+          )})
+        }
+       
+  </ul>
+    
+   </div>
+  )
 }
 export default App
---
+++ src/navbar.js
@@ -0,0 +1,13 @@
+function Navbar()
+{
+    return(
+       <div id="nav">
+        <a href="">home</a>
+        <a href="">contact us</a> 
+        <a href="">about us</a>
+        <a href="">feedback</a>
+
+       </div>    
+    )
+}
+export default Navbar
